311e43fab32ec40a2315aabde66d6699b958dfbf
Desirable-Difficulties <KaiHarmer@desirabledifficulties.onmicrosoft.com>
2025-10-02 01:10:01 +0000

    Implement Exam 1 question solutions

--- .vscode/EXAM 1/HARMER_KAI_U0895215_E01.py
+++ .vscode/EXAM 1/HARMER_KAI_U0895215_E01.py
@@ -15,9 +15,9 @@ import yfinance as yf
 ### Your function has a single input
 ### Determine if an input is an integer. Return the appropriate boolean statement for each input. 
 
-def question_1():
+def question_1(input_one):
 
-    return
+    return isinstance(input_one, int)
 
 ######################
 ##### Question 2 #####
@@ -26,9 +26,16 @@ def question_1():
 ## Create a dictionary object with input one as your keys and input two as your values
 ## Then return the key/value pair at index four (4) as a tuple. 
 
-def question_2():
-    
-    return
+def question_2(input_one, input_two):
+
+    new_dict = {}
+
+    for index in range(len(input_one)):
+        new_dict[input_one[index]] = input_two[index]
+
+    items = list(new_dict.items())
+
+    return items[4]
 
 ######################
 ##### Question 3 #####
@@ -38,9 +45,12 @@ def question_2():
 ## Determine if the first and third inputs are equal, save this result as a variable
 ## Compare the two saved variables, determine if they are the same value, then return the outcome of this comparison. 
 
-def question_3():
-    
-    return
+def question_3(input_one, input_two, input_three):
+
+    all_equal = input_one == input_two == input_three
+    first_third_equal = input_one == input_three
+
+    return all_equal == first_third_equal
 
 ######################
 ##### Question 4 #####
@@ -51,9 +61,14 @@ def question_3():
 ## Update the counter value once per loop by adding three to the current value. 
 ## When the loop is finished, return the counter. 
 
-def question_4():
-    
-    return
+def question_4(input_one):
+
+    counter = 0
+
+    for _ in range(input_one):
+        counter = counter + 3
+
+    return counter
 
 ######################
 ##### Question 5 #####
@@ -64,9 +79,14 @@ def question_4():
 ## Inside the for loop, update the output variable by adding each dictionary value to it
 ## When the for loop is finished, return the output variable. 
 
-def question_5():
-    
-    return
+def question_5(input_one):
+
+    output = 10
+
+    for value in input_one.values():
+        output = output + value
+
+    return output
 
 ######################
 ##### Question 6 #####
@@ -84,9 +104,15 @@ def question_5():
     ## Add all new variables together, multiply them by 4, then divide by 7.
 ## Return output. 
 
-def question_6():
-    
-    return  
+def question_6(input_one):
+
+    variable1 = input_one['KEY_3']['INNER_KEY_4']
+    variable2 = input_one['KEY_1']['INNER_KEY_2']
+    variable3 = input_one['KEY_4']['INNER_KEY_3']
+
+    output = (variable1 + variable2 + variable3) * 4 / 7
+
+    return output 
 
 ######################
 ##### Question 7 #####
@@ -100,9 +126,14 @@ def question_6():
     ## Add each loop's result into the output list.
 ## When the for loop is finished, return the output variable. 
 
-def question_7():
-    
-    return
+def question_7(input_one):
+
+    output = []
+
+    for i in range(input_one):
+        output.append(i * 47)
+
+    return output
 
 ######################
 ##### Question 8 #####
@@ -115,9 +146,17 @@ def question_7():
     ## else, set the input two to equal the value of the first input. 
 ## return the updated version of input 2 when the loop is finished. 
 
-def question_8():
-    
-    return
+def question_8(input_one, input_two, input_three):
+
+    for i in input_three:
+        if i > input_one:
+            input_two = input_two + 3
+        elif i < input_one:
+            input_one = input_one + 3
+        else:
+            input_two = input_one
+
+    return input_two
 
 ######################
 ##### Question 9 #####
@@ -129,9 +168,14 @@ def question_8():
 ## Update the output variable again to be equal to itself divided by 5.
 ## Return the output variable. 
 
-def question_9():
-    
-    return
+def question_9(input_one):
+
+    output = input_one.pop('TARGET')
+    output = output * 10
+    output = output + 2
+    output = output / 5
+
+    return output
 
 #######################
 ##### Question 10 #####
@@ -145,9 +189,17 @@ def question_9():
 
 ## After the loop, return the sum of the output list. 
 
-def question_10():
-    
-    return
+def question_10(input_one, input_two):
+
+    output = []
+
+    for i in input_one:
+        if i > input_two:
+            output.append(False)
+        else:
+            output.append(True)
+
+    return sum(output)
 
 #######################
 ##### Question 11 #####
@@ -160,9 +212,24 @@ def question_10():
             ## If the value is equal to 25, then return the input as is, otherwise enter another code block. 
                 ## If the value is less than 30 and greater than 20, return the input * 50, else return zero (0)
 
-def question_11():
-    
-    return
+def question_11(input_one):
+
+    if input_one > 10:
+        if input_one < 50:
+            if input_one > 15 and input_one < 35:
+                if input_one == 25:
+                    return input_one
+                else:
+                    if input_one < 30 and input_one > 20:
+                        return input_one * 50
+                    else:
+                        return 0
+            else:
+                return input_one / 5
+        else:
+            return input_one * 10
+    else:
+        return input_one
 
 #######################
 ##### Question 12 #####
@@ -172,9 +239,9 @@ def question_11():
     ## "Input1: X, Input2: Y, Input3: Z"
 ## The X, Y, and Z, should be filled with the inputs provided to the function. 
 
-def question_12():
-    
-    return
+def question_12(input_one, input_two, input_three):
+
+    return f"Input1: {input_one}, Input2: {input_two}, Input3: {input_three}"
 
 #######################
 ##### Question 13 #####
@@ -193,9 +260,19 @@ def question_12():
     ## "The final value is X"
 ## X should be the value of output. 
 
-def question_13():
-    
-    return
+def question_13(input_one, input_two, input_three):
+
+    if input_one > 20:
+        if input_two < 10:
+            input_three = input_three * input_one
+        else:
+            if input_three > 100:
+                input_one = 600 * input_two
+
+    output = input_two
+    output = (input_one * input_three + input_two) / output
+
+    return f"The final value is {output}"
 
 #######################
 ##### Question 14 #####
@@ -211,9 +288,22 @@ def question_13():
 ## After running the loops, divide each list value of "output" by 4, then reassign the result to itself. 
 ## Multiply the output list by the counter variable, then return the result.
 
-def question_14():
-    
-    return
+def question_14(input_one, input_two):
+
+    output = []
+    counter = 0
+
+    for i in input_one:
+        for j in input_two:
+            counter = counter + 1
+            output.append(i * j)
+
+    for index in range(len(output)):
+        output[index] = output[index] / 4
+
+    output = output * counter
+
+    return output
 
 #######################
 ##### Question 15 #####
@@ -231,9 +321,15 @@ def question_14():
         ## Your value will be input two times i.
 ## Finally, return the output variable. 
 
-def question_15():
-    
-    return
+def question_15(input_one, input_two):
+
+    output = {}
+
+    for i in range(input_one):
+        key = "KEY" + str(i)
+        output[key] = input_two * i
+
+    return output
 
 #######################
 ##### Question 16 #####
@@ -246,9 +342,15 @@ def question_15():
         ## Update "output" with the value associated with "TARGET"
 ## Return the output object
 
-def question_16():
-    
-    return
+def question_16(input_one):
+
+    output = 0
+
+    for key, value in input_one.items():
+        if key == 'TARGET':
+            output = value
+
+    return output
 
 #######################
 ##### Question 17 #####
@@ -265,9 +367,29 @@ def question_16():
 ## Select the most recent 200 rows of data and save the result to output. 
 ## Return the output dataframe. 
 
-def question_17():
-    
-    return
+def question_17(ticker, beginning_date, ending_date):
+
+    output = yf.download(ticker, start=beginning_date, end=ending_date, auto_adjust=True, progress=False)
+
+    if output.empty:
+        return output
+
+    output.index = pd.to_datetime(output.index)
+    output.index.name = None
+
+    if isinstance(ticker, str):
+        output = output[['Close']].copy()
+        output.columns = [ticker]
+    else:
+        if isinstance(output.columns, pd.MultiIndex):
+            output = output['Close'].copy()
+        else:
+            output = output[['Close']].copy()
+
+    output.columns.name = 'Ticker'
+    output = output.tail(200)
+
+    return output
 
 #######################
 ##### Question 18 #####
@@ -277,9 +399,15 @@ def question_17():
 ## Find the index location of the value and save this to "max_index". Do not use .idxmax(). 
 ## return max_value and max_index as a tuple
 
-def question_18():
-    
-    return
+def question_18(input_one):
+
+    max_value = input_one.max()
+    if hasattr(max_value, 'item'):
+        max_value = max_value.item()
+    values_list = input_one.tolist()
+    max_index = values_list.index(max_value)
+
+    return (max_value, max_index)
 
 #######################
 ##### Question 19 #####
@@ -294,9 +422,16 @@ def question_18():
 ## Create another column called "NEW_MEAN" and fill it with the average value of 'COL_2'
 ## Return the output dataframe. 
 
-def question_19():
-    
-    return
+def question_19(input_one):
+
+    output = pd.DataFrame(input_one)
+    output['NEW_COL'] = output['COL_1'].median()
+    output['ANOTHER_COL'] = 1
+
+    output = output.iloc[2:6, 1:4].copy()
+    output['NEW_MEAN'] = output['COL_2'].mean()
+
+    return output
 
 #######################
 ##### Question 20 #####
@@ -310,6 +445,17 @@ def question_19():
 
 ## Return the output dataframe
 
-def question_20():
-    
-    return
+def question_20(input_one, input_two, input_three, input_four):
+
+    output = pd.DataFrame({
+        'COL_1': input_one,
+        'COL_2': input_two,
+        'COL_3': input_three,
+        'COL_4': input_four
+    })
+
+    output['COL_1'] = output['COL_1'] * output['COL_1']
+    output.drop('COL_4', axis=1, inplace=True)
+    output['NEW_COL'] = output['COL_3'] ** output['COL_2']
+
+    return output
